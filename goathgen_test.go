f7b08ed9e463a96d7694619afed2e1dd614238ba
w8rbt <brad@w8rbt.org>
2017-04-20 16:17:49 -0400

    Made adding tests easier

--- goathgen_test.go
+++ goathgen_test.go
@@ -20,34 +20,90 @@ import "fmt"
 import "testing"
 import "github.com/w8rbt/goathgen"
 
-func Test1(t *testing.T) {
-	binary_value := goathgen.ToBinary(goathgen.Totp("GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ", 59, 30, 0))
-	if binary_value != 1094287082 {
-		t.Error("Expected bincode 1094287082, got", binary_value)
+var b32_secret string = "GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ"
+var bincode int = 0
+var hex_secret string = "3132333435363738393031323334353637383930"
+var seconds int64 = 0
+var time_step int64 = 30
+var unix_epoch int64 = 0
+var totp string = ""
+
+// ---------
+
+func Test1a(t *testing.T) {
+	bincode = 1094287082
+	seconds = 59
+	totp = "287082"
+	binary_value := goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch))
+	if binary_value != bincode {
+		t.Error("Expected bincode", bincode, "got", binary_value)
 	}
 }
 
-func Test2(t *testing.T) {
-	truncated_value := goathgen.Truncate(goathgen.ToBinary(goathgen.Totp("GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ", 59, 30, 0)))
+func Test1b(t *testing.T) {
+	truncated_value := goathgen.Truncate(goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch)))
 	final_value := fmt.Sprintf("%06d", truncated_value)
-	if final_value != "287082" {
-		t.Error("Expected OTP 287082, got", final_value)
+	if final_value != totp {
+		t.Error("Expected totp", totp, "got", final_value)
 	}
 }
 
 // ---------
 
-func Test3(t *testing.T) {
-	binary_value := goathgen.ToBinary(goathgen.Totp("3132333435363738393031323334353637383930", 1111111109, 30, 0))
-	if binary_value != 907081804 {
-		t.Error("Expected bincode 907081804, got", binary_value)
+func Test2a(t *testing.T) {
+	bincode = 907081804
+	seconds = 1111111109
+	totp = "081804"
+	binary_value := goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch))
+	if binary_value != bincode {
+		t.Error("Expected bincode", bincode, "got", binary_value)
 	}
 }
 
-func Test4(t *testing.T) {
-	truncated_value := goathgen.Truncate(goathgen.ToBinary(goathgen.Totp("3132333435363738393031323334353637383930", 1111111109, 30, 0)))
+func Test2b(t *testing.T) {
+	truncated_value := goathgen.Truncate(goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch)))
 	final_value := fmt.Sprintf("%06d", truncated_value)
-	if final_value != "081804" {
-		t.Error("Expected OTP 081804, got", final_value)
+	if final_value != totp {
+		t.Error("Expected totp", totp, "got", final_value)
+	}
+}
+
+// ---------
+
+func Test3a(t *testing.T) {
+	bincode = 414050471
+	seconds = 1111111111
+	totp = "050471"
+	binary_value := goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch))
+	if binary_value != bincode {
+		t.Error("Expected bincode", bincode, "got", binary_value)
+	}
+}
+
+func Test3b(t *testing.T) {
+	truncated_value := goathgen.Truncate(goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch)))
+	final_value := fmt.Sprintf("%06d", truncated_value)
+	if final_value != totp {
+		t.Error("Expected totp", totp, "got", final_value)
+	}
+}
+
+// ---------
+
+func Test4a(t *testing.T) {
+	bincode = 689005924
+	seconds = 1234567890
+	totp = "005924"
+	binary_value := goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch))
+	if binary_value != bincode {
+		t.Error("Expected bincode", bincode, "got", binary_value)
+	}
+}
+
+func Test4b(t *testing.T) {
+	truncated_value := goathgen.Truncate(goathgen.ToBinary(goathgen.Totp(b32_secret, seconds, time_step, unix_epoch)))
+	final_value := fmt.Sprintf("%06d", truncated_value)
+	if final_value != totp {
+		t.Error("Expected totp", totp, "got", final_value)
 	}
 }
